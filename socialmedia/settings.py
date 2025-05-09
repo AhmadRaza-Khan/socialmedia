fe2fbd3e52e56bba83ba24746f0382f0524f46bc
AhmadRaza-Khan <ahmadrazakhanbwn@gmail.com>
2025-05-09 23:02:04 +0500

    some fixes

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -88,9 +88,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'socialmedia.wsgi.application'
 ASGI_APPLICATION = 'socialmedia.asgi.application'
 
+
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
     },
 }
 
