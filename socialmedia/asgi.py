888c9df296ea29c62d1982f6695894f35304382f
AhmadRaza-Khan <ahmadrazakhanbwn@gmail.com>
2025-05-09 23:12:43 +0500

    some changes

--- socialmedia/asgi.py
+++ socialmedia/asgi.py
@@ -17,7 +17,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialmedia.settings')
 
 application = ProtocolTypeRouter(
     {
-        "https" : get_asgi_application() , 
+        "http" : get_asgi_application() , 
         "websocket" : AuthMiddlewareStack(
             URLRouter(
                 routing.websocket_urlpatterns
